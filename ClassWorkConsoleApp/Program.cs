df573fa3a94bd7b258ba2b8a3884166a307cb2c6
Amber Mills <amberdmills@live.com>
2024-11-14 12:42:05 -0600

    Add project files.

---
+++ ClassWorkConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿Random random = new Random(2);
+
+Console.Write("Enter the number of coin flips: ");
+int numFlips = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numFlips; ++i)
+    Console.WriteLine(CoinFlip(random));
+
+string CoinFlip(Random random)
+{
+    int result = random.Next(2);
+    return (result == 1) ? "Heads" : "Tails";
+}
